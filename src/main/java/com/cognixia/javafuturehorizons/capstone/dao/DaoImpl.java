f433163fea95210f53a9905f0c1531dbd1e8672f
ilathem <isaiahlathem@gmail.com>
2025-06-27 12:42:17 -0500

    open and close connection per transaction

--- src/main/java/com/cognixia/javafuturehorizons/capstone/dao/DaoImpl.java
+++ src/main/java/com/cognixia/javafuturehorizons/capstone/dao/DaoImpl.java
@@ -1,6 +1,5 @@
 package com.cognixia.javafuturehorizons.capstone.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -17,14 +16,12 @@ import com.cognixia.javafuturehorizons.capstone.model.UserProgress;
 
 public class DaoImpl implements Dao {
 
-  private Connection connection = null;
   private static DaoImpl instance;
 
-  private DaoImpl() throws SQLException, ClassNotFoundException {
-    establishConnection();
+  private DaoImpl() {
   }
 
-  public static DaoImpl getInstance() throws ClassNotFoundException, SQLException {
+  public static DaoImpl getInstance() {
     if (instance == null) {
       instance = new DaoImpl();
     }
@@ -32,261 +29,267 @@ public class DaoImpl implements Dao {
   }
 
   @Override
-  public void establishConnection() throws SQLException, ClassNotFoundException {
-    if (connection == null) {
-      connection = ConnectionManager.getConnection();
-    }
-  }
-
-  @Override
-  public void closeConnection() throws SQLException {
-    if (connection != null)
-      connection.close();
-  }
-
-  @Override
-  public User createUser(User user) throws SQLException {
+  public User createUser(User user) throws SQLException, ClassNotFoundException {
     String sql = "INSERT INTO users (name, password) VALUES (?, ?)";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setString(1, user.getName());
-    stmt.setString(2, user.getPassword());
-    stmt.executeUpdate();
-    return this.validateUser(user.getName(), user.getPassword()).get();
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setString(1, user.getName());
+      stmt.setString(2, user.getPassword());
+      stmt.executeUpdate();
+      return this.validateUser(user.getName(), user.getPassword()).get();
+    }
   }
 
   @Override
-  public boolean deleteUser(User user) throws SQLException, UserNotFoundException {
+  public boolean deleteUser(User user) throws SQLException, UserNotFoundException, ClassNotFoundException {
     String sql = "DELETE FROM users WHERE name = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setString(1, user.getName());
-    int rowsAffected = stmt.executeUpdate();
-    return rowsAffected > 0;
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setString(1, user.getName());
+      int rowsAffected = stmt.executeUpdate();
+      return rowsAffected > 0;
+    }
   }
 
   @Override
-  public Optional<User> getUserById(int userId) throws SQLException, UserNotFoundException {
+  public Optional<User> getUserById(int userId) throws SQLException, UserNotFoundException, ClassNotFoundException {
     String sql = "SELECT * FROM users WHERE user_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, userId);
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      User user = new User(
-          resultSet.getInt("user_id"),
-          resultSet.getInt("clearance"),
-          resultSet.getString("name"),
-          resultSet.getString("password"));
-      return Optional.of(user);
-    } else {
-      throw new UserNotFoundException("User with ID " + userId + " not found.");
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, userId);
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        User user = new User(
+            resultSet.getInt("user_id"),
+            resultSet.getInt("clearance"),
+            resultSet.getString("name"),
+            resultSet.getString("password"));
+        return Optional.of(user);
+      } else {
+        throw new UserNotFoundException("User with ID " + userId + " not found.");
+      }
     }
   }
 
   @Override
-  public Optional<User> validateUser(String username, String password) throws SQLException {
+  public Optional<User> validateUser(String username, String password) throws SQLException, ClassNotFoundException {
     String sql = "SELECT * FROM users WHERE name = ? AND password = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setString(1, username);
-    stmt.setString(2, password);
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      User user = new User(
-          resultSet.getInt("user_id"),
-          resultSet.getInt("clearance"),
-          resultSet.getString("name"),
-          resultSet.getString("password"));
-      return Optional.of(user);
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setString(1, username);
+      stmt.setString(2, password);
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        User user = new User(
+            resultSet.getInt("user_id"),
+            resultSet.getInt("clearance"),
+            resultSet.getString("name"),
+            resultSet.getString("password"));
+        return Optional.of(user);
+      }
+      return Optional.empty();
     }
-    return Optional.empty();
   }
 
   @Override
-  public List<Book> getAllBooks() throws SQLException {
+  public List<Book> getAllBooks() throws SQLException, ClassNotFoundException {
     List<Book> books = new ArrayList<>();
     String sql = "SELECT * FROM books";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    var resultSet = stmt.executeQuery();
-    while (resultSet.next()) {
-      Book book = new Book(
-          resultSet.getInt("book_id"),
-          resultSet.getString("title"),
-          resultSet.getString("author"),
-          resultSet.getInt("num_pages"));
-      books.add(book);
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      var resultSet = stmt.executeQuery();
+      while (resultSet.next()) {
+        Book book = new Book(
+            resultSet.getInt("book_id"),
+            resultSet.getString("title"),
+            resultSet.getString("author"),
+            resultSet.getInt("num_pages"));
+        books.add(book);
+      }
+      return books;
     }
-    return books;
   }
 
   @Override
-  public Optional<Book> getBookById(int bookId) throws SQLException, BookNotFoundException {
+  public Optional<Book> getBookById(int bookId) throws SQLException, BookNotFoundException, ClassNotFoundException {
     String sql = "SELECT * FROM books WHERE book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, bookId);
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      Book book = new Book(
-          resultSet.getInt("book_id"),
-          resultSet.getString("title"),
-          resultSet.getString("author"),
-          resultSet.getInt("num_pages"));
-      return Optional.of(book);
-    } else {
-      throw new BookNotFoundException("Book with ID " + bookId + " not found.");
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, bookId);
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        Book book = new Book(
+            resultSet.getInt("book_id"),
+            resultSet.getString("title"),
+            resultSet.getString("author"),
+            resultSet.getInt("num_pages"));
+        return Optional.of(book);
+      } else {
+        throw new BookNotFoundException("Book with ID " + bookId + " not found.");
+      }
     }
   }
 
   @Override
-  public boolean addBook(Book book) throws SQLException {
+  public boolean addBook(Book book) throws SQLException, ClassNotFoundException {
     String sql = "INSERT INTO books (title, author, num_pages) VALUES (?, ?, ?)";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setString(1, book.getTitle());
-    stmt.setString(2, book.getAuthor());
-    stmt.setInt(3, book.getNumPages());
-    int rowsAffected = stmt.executeUpdate();
-    return rowsAffected > 0;
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setString(1, book.getTitle());
+      stmt.setString(2, book.getAuthor());
+      stmt.setInt(3, book.getNumPages());
+      int rowsAffected = stmt.executeUpdate();
+      return rowsAffected > 0;
+    }
   }
 
   @Override
-  public boolean updateBook(Book book) throws SQLException, BookNotFoundException {
+  public boolean updateBook(Book book) throws SQLException, BookNotFoundException, ClassNotFoundException {
     String sql = "UPDATE books SET title = ?, author = ?, num_pages = ? WHERE book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setString(1, book.getTitle());
-    stmt.setString(2, book.getAuthor());
-    stmt.setInt(3, book.getNumPages());
-    stmt.setInt(4, book.getBookId());
-    int rowsAffected = stmt.executeUpdate();
-    if (rowsAffected > 0) {
-      return true;
-    } else {
-      throw new BookNotFoundException("Book with ID " + book.getBookId() + " not found.");
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setString(1, book.getTitle());
+      stmt.setString(2, book.getAuthor());
+      stmt.setInt(3, book.getNumPages());
+      stmt.setInt(4, book.getBookId());
+      int rowsAffected = stmt.executeUpdate();
+      if (rowsAffected > 0) {
+        return true;
+      } else {
+        throw new BookNotFoundException("Book with ID " + book.getBookId() + " not found.");
+      }
     }
   }
 
   @Override
-  public boolean deleteBook(Book book) throws SQLException, BookNotFoundException {
+  public boolean deleteBook(Book book) throws SQLException, BookNotFoundException, ClassNotFoundException {
     String sql = "DELETE FROM books WHERE book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, book.getBookId());
-    int rowsAffected = stmt.executeUpdate();
-    if (rowsAffected > 0) {
-      return true;
-    } else {
-      throw new BookNotFoundException("Book with ID " + book.getBookId() + " not found.");
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, book.getBookId());
+      int rowsAffected = stmt.executeUpdate();
+      if (rowsAffected > 0) {
+        return true;
+      } else {
+        throw new BookNotFoundException("Book with ID " + book.getBookId() + " not found.");
+      }
     }
   }
 
   @Override
-  public List<Tracker> getUserProgress(User user) throws SQLException, UserNotFoundException {
+  public List<Tracker> getUserProgress(User user) throws SQLException, UserNotFoundException, ClassNotFoundException {
     List<Tracker> trackers = new ArrayList<>();
     String sql = "SELECT b.book_id, b.title, b.author, b.num_pages, t.progress, t.rating " +
         "FROM books b JOIN trackers t ON b.book_id = t.book_id " +
         "WHERE t.user_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, user.getUserId());
-    var resultSet = stmt.executeQuery();
-    while (resultSet.next()) {
-      Book book = new Book(
-          resultSet.getInt("book_id"),
-          resultSet.getString("title"),
-          resultSet.getString("author"),
-          resultSet.getInt("num_pages"));
-      int progress = resultSet.getInt("progress");
-      int rating = resultSet.getInt("rating");
-      trackers.add(new Tracker(book, progress, rating));
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, user.getUserId());
+      var resultSet = stmt.executeQuery();
+      while (resultSet.next()) {
+        Book book = new Book(
+            resultSet.getInt("book_id"),
+            resultSet.getString("title"),
+            resultSet.getString("author"),
+            resultSet.getInt("num_pages"));
+        int progress = resultSet.getInt("progress");
+        int rating = resultSet.getInt("rating");
+        trackers.add(new Tracker(book, progress, rating));
+      }
+      return trackers;
     }
-    return trackers;
   }
 
   @Override
-  public Optional<Tracker> getUserProgress(User user, Book book) throws SQLException, UserNotFoundException {
+  public Optional<Tracker> getUserProgress(User user, Book book)
+      throws SQLException, UserNotFoundException, ClassNotFoundException {
     String sql = "select progress, rating from trackers where user_id = ? and book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, user.getUserId());
-    stmt.setInt(2, book.getBookId());
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      int progress = resultSet.getInt("progress");
-      int rating = resultSet.getInt("rating");
-      return Optional.of(new Tracker(book, progress, rating));
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, user.getUserId());
+      stmt.setInt(2, book.getBookId());
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        int progress = resultSet.getInt("progress");
+        int rating = resultSet.getInt("rating");
+        return Optional.of(new Tracker(book, progress, rating));
+      }
+      return Optional.empty();
     }
-    return Optional.empty();
   }
 
   @Override
-  public List<UserProgress> getAllUsersProgress(Book book) throws SQLException, BookNotFoundException {
+  public List<UserProgress> getAllUsersProgress(Book book)
+      throws SQLException, BookNotFoundException, ClassNotFoundException {
     String sql = "SELECT u.name, t.progress, t.rating " +
         "FROM users u JOIN trackers t ON u.user_id = t.user_id " +
         "WHERE t.book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, book.getBookId());
-    var resultSet = stmt.executeQuery();
-    List<UserProgress> userProgressList = new ArrayList<>();
-    while (resultSet.next()) {
-      String username = resultSet.getString("name");
-      int progress = resultSet.getInt("progress");
-      int rating = resultSet.getInt("rating");
-      userProgressList.add(new UserProgress(username, progress, rating));
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, book.getBookId());
+      var resultSet = stmt.executeQuery();
+      List<UserProgress> userProgressList = new ArrayList<>();
+      while (resultSet.next()) {
+        String username = resultSet.getString("name");
+        int progress = resultSet.getInt("progress");
+        int rating = resultSet.getInt("rating");
+        userProgressList.add(new UserProgress(username, progress, rating));
+      }
+      return userProgressList;
     }
-    return userProgressList;
   }
 
   @Override
-  public boolean updateProgress(User user, Book book, int progress) throws SQLException {
+  public boolean updateProgress(User user, Book book, int progress) throws SQLException, ClassNotFoundException {
     String sql = "INSERT INTO trackers (user_id, book_id, progress) VALUES (?, ?, ?) " +
         "ON DUPLICATE KEY UPDATE progress = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, user.getUserId());
-    stmt.setInt(2, book.getBookId());
-    stmt.setInt(3, progress);
-    stmt.setInt(4, progress);
-    stmt.executeUpdate();
-    int rowsAffected = stmt.executeUpdate();
-    return rowsAffected > 0;
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, user.getUserId());
+      stmt.setInt(2, book.getBookId());
+      stmt.setInt(3, progress);
+      stmt.setInt(4, progress);
+      stmt.executeUpdate();
+      int rowsAffected = stmt.executeUpdate();
+      return rowsAffected > 0;
+    }
   }
 
   @Override
   public boolean rateBook(User user, Book book, int rating)
-      throws SQLException {
+      throws SQLException, ClassNotFoundException {
     String sql = "INSERT INTO trackers (user_id, book_id, rating) VALUES (?, ?, ?) " +
         "ON DUPLICATE KEY UPDATE rating = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, user.getUserId());
-    stmt.setInt(2, book.getBookId());
-    stmt.setInt(3, rating);
-    stmt.setInt(4, rating);
-    int rowsAffected = stmt.executeUpdate();
-    if (rowsAffected > 0) {
-      return true;
-    } else {
-      throw new SQLException("Failed to rate book. User or book not found.");
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, user.getUserId());
+      stmt.setInt(2, book.getBookId());
+      stmt.setInt(3, rating);
+      stmt.setInt(4, rating);
+      int rowsAffected = stmt.executeUpdate();
+      if (rowsAffected > 0) {
+        return true;
+      } else {
+        throw new SQLException("Failed to rate book. User or book not found.");
+      }
     }
   }
 
   @Override
-  public Optional<Double> getAverageRating(Book book) throws SQLException, BookNotFoundException {
+  public Optional<Double> getAverageRating(Book book)
+      throws SQLException, BookNotFoundException, ClassNotFoundException {
     String sql = "SELECT AVG(rating) AS average_rating FROM trackers WHERE book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, book.getBookId());
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      double averageRating = resultSet.getDouble("average_rating");
-      return Optional.of(averageRating);
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, book.getBookId());
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        double averageRating = resultSet.getDouble("average_rating");
+        return Optional.of(averageRating);
+      }
+      return Optional.empty();
     }
-    return Optional.empty();
   }
 
   @Override
   public Optional<Integer> getUserRating(User user, Book book)
-      throws SQLException {
+      throws SQLException, ClassNotFoundException {
     String sql = "SELECT rating FROM trackers WHERE user_id = ? AND book_id = ?";
-    PreparedStatement stmt = connection.prepareStatement(sql);
-    stmt.setInt(1, user.getUserId());
-    stmt.setInt(2, book.getBookId());
-    var resultSet = stmt.executeQuery();
-    if (resultSet.next()) {
-      int rating = resultSet.getInt("rating");
-      return Optional.of(rating);
+    try (PreparedStatement stmt = ConnectionManager.getConnection().prepareStatement(sql)) {
+      stmt.setInt(1, user.getUserId());
+      stmt.setInt(2, book.getBookId());
+      var resultSet = stmt.executeQuery();
+      if (resultSet.next()) {
+        int rating = resultSet.getInt("rating");
+        return Optional.of(rating);
+      }
+      return Optional.empty();
     }
-    return Optional.empty();
   }
 
 }
